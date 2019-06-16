85cb29dae5089cfd161a3d8f519dd0d3307859f2
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-16 20:40:21 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+a = int(input("enter num 1 :"))
+b = int(input("enter num 2 :"))
+def sub(num1 , num2):
+    my_sub = num1 - num2 
+    print(my_sub)
+sub(a,b)
+
+
+def div(num1 , num2):
+    my_div = num1 / num2
+    print(my_div)
+div(a,b)
+
+
+def mul(num1 , num2):
+    my_mul = num1 * num2
+    print(my_mul)
+mul(a,b)
