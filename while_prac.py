f39c5c803ae05d9eba180ab1e0175e84b62f2fb6
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-23 17:14:30 +0500

    practice

---
+++ while_prac.py
@@ -0,0 +1,6 @@
+import random
+temp = random.randint(30, 50)
+while temp < 45:
+    print('it\'s better weather, thep=', temp)
+    temp = random.randint(30, 50)
+print('i came back due to hot weather, temp=', temp)
