dcbd87d39fc29748388639c44b33ef0a99243804
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-07-14 17:32:31 +0500

    2. Farm problem

---
+++ farm_prob.py
@@ -0,0 +1,13 @@
+#Question num 3:
+
+''' The Farm Problem  '''
+
+x = int(input('enter num of chikens:'))
+a = x*2
+y = int(input('enter num of cows:'))
+b = y*4
+z = int(input('enter num of pigs:'))
+c = z*4
+
+i = a+b+c
+print(i)
