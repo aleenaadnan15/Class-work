3cce02d2d0117f3c0cb59ce15cd9c5557445a21c
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-07-15 01:03:52 +0500

    5. length b/w two strings

---
+++ sec_str.py
@@ -0,0 +1,11 @@
+#Question num 5:
+
+''' Create a function that takes two string '''
+
+strings1 = input('enter first string:')
+strings2 = input('enter second string:')
+if len(strings1) == len(strings2):
+    print('true')
+else:
+    strings1 != strings2
+    print('false')
